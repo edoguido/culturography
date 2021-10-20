4edf13ad4b80637410cbb6586af685c3a3361ce4
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-10-20 20:55:04 +0200

    feat: add basic layout structure, styles and components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
+import { Fragment } from 'react'
+import { ThemeProvider } from 'styled-components'
 import '../styles/globals.css'
 
+import { GlobalStyle } from '../lib/utils/theme'
+import theme from '../public/design-tokens.json'
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Fragment>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Fragment>
+  )
 }
 
 export default MyApp
