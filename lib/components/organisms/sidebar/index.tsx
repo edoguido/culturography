794e3495ea4592413ec4824c6201cb0393bb8307
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-12-07 00:33:37 +0100

    feat: render story content in sidebar and update state on scroll

--- lib/components/organisms/sidebar/index.tsx
+++ lib/components/organisms/sidebar/index.tsx
@@ -1,21 +1,138 @@
-import * as Styled from './styled'
+import {
+  createRef,
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react'
+import BlockContent from '@sanity/block-content-to-react'
 
 import { useVizLayout } from '@/context/vizLayoutContext'
 import { motionOptions } from '@/const/motionProps'
 
+import SidebarChapterSelector from 'components/molecules/sidebarChapterSelector'
+import * as Styled from './styled'
+
 const Sidebar = () => {
-  const [layout] = useVizLayout()
+  const [state, dispatch] = useVizLayout()
+  const storyRef = useRef<HTMLDivElement>(null)
+  const [chapterIndex, setChapterIndex] = useState(null)
+
+  const {
+    story: { data },
+  } = state
+
+  const chapterRefs = useMemo(
+    () => state.story.data.story_chapters.map(() => createRef()),
+    []
+  )
+
+  const onScroll = useCallback(() => {
+    window.requestAnimationFrame(onScroll)
+
+    const checkChapters = (scrollTop) => {
+      chapterRefs.map((ref, i) => {
+        if (!ref.current) return
+
+        const refTop = ref.current.scrollTop
+        const refBottom = refTop + ref.current.scrollHeight
+
+        if (scrollTop > refTop && scrollTop < refBottom) {
+          setChapterIndex(i)
+          return
+        }
+      })
+    }
+
+    if (!storyRef.current) return
+
+    const scrollTop = storyRef.current.scrollTop
+
+    checkChapters(scrollTop)
+  }, [chapterRefs])
+
+  useEffect(() => {
+    window.requestAnimationFrame(onScroll)
+  }, [])
+
+  useEffect(() => {
+    if (chapterIndex === null) return
+
+    const { left_network_shapefile, right_network_shapefile } =
+      data.story_chapters[chapterIndex]
+
+    dispatch({
+      type: 'UPDATE_STORY_CHAPTER',
+      payload: {
+        story: { chapter: chapterIndex },
+        clusters: {
+          left: left_network_shapefile,
+          right: right_network_shapefile,
+        },
+      },
+    })
+  }, [chapterIndex])
 
   return (
     <Styled.SidebarWrapper
+      ref={storyRef}
       animate={{
-        x: layout.read ? 0 : layout.sidebarWidth.value,
+        x: state.read ? 0 : state.sidebarWidth.value,
       }}
       transition={motionOptions}
     >
-      Sidebar
+      <Styled.SidebarContent>
+        <SidebarChapterSelector />
+        {data.story_chapters && (
+          <Styled.SidebarStoryContent>
+            {data.story_chapters.map(
+              ({ chapter_title, chapter_content }, i: number) => {
+                return (
+                  <Styled.SidebarStoryChapterWrapper
+                    key={i}
+                    ref={chapterRefs[i]}
+                  >
+                    <Styled.SidebarStoryChapterTitle>
+                      {chapter_title}
+                    </Styled.SidebarStoryChapterTitle>
+                    {chapter_content.map(
+                      (
+                        { block_title, block_content, ref: chapterContentRef },
+                        j: number
+                      ) => {
+                        return (
+                          <Styled.SidebarStoryBlockWrapper key={j}>
+                            <Styled.SidebarStoryBlockTitle>
+                              {block_title}
+                            </Styled.SidebarStoryBlockTitle>
+                            {block_content.map((c, t: number) => (
+                              <Styled.SidebarStoryBlockContent
+                                key={t}
+                                ref={chapterContentRef}
+                              >
+                                <BlockContent blocks={c} />
+                              </Styled.SidebarStoryBlockContent>
+                            ))}
+                          </Styled.SidebarStoryBlockWrapper>
+                        )
+                      }
+                    )}
+                  </Styled.SidebarStoryChapterWrapper>
+                )
+              }
+            )}
+          </Styled.SidebarStoryContent>
+        )}
+      </Styled.SidebarContent>
     </Styled.SidebarWrapper>
   )
+}
+
+const StoryChapter = () => {
+  const chapterRef = useRef()
+
+  return
 }
 
 export default Sidebar
--- lib/components/organisms/sidebar/styled.tsx
+++ lib/components/organisms/sidebar/styled.tsx
@@ -1,14 +1,40 @@
 import { motion } from 'framer-motion'
 import styled from 'styled-components'
 
+export const SidebarStoryBlockContent = styled.div`
+  margin-bottom: 1.5rem;
+`
+
+export const SidebarStoryBlockTitle = styled.h3``
+
+export const SidebarStoryBlockWrapper = styled.div``
+
+export const SidebarStoryChapterTitle = styled.h2``
+
+export const SidebarStoryChapterWrapper = styled.div``
+
+export const SidebarStoryContent = styled.div`
+  max-width: var(--sidebar-width);
+  overflow-x: auto;
+
+  padding: 0.85rem;
+`
+
+export const SidebarContent = styled.div`
+  /* padding-top: var(--nav-height); */
+`
+
 export const SidebarWrapper = styled(motion.div)`
+  z-index: 100;
+
   position: fixed;
   right: 0;
-  top: 0;
+  top: var(--nav-height);
   bottom: 0;
-  width: var(--sidebar-width);
 
-  padding-top: var(--nav-height);
+  width: var(--sidebar-width);
+  max-height: 100vh;
 
   background-color: white;
+  overflow-y: auto;
 `
