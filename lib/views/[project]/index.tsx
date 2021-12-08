157626bffc93222a51e81e8df25d8f6c82dad400
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-12-08 23:28:09 +0100

    feat: load test data on page build

--- lib/views/[project]/index.tsx
+++ lib/views/[project]/index.tsx
@@ -96,11 +96,21 @@ export async function getStaticPaths() {
   }
 }
 
+import Graph from 'graphology'
+import gexf from 'graphology-gexf'
+
 export async function getStaticProps({ params: { project } }) {
   const query = PROJECT_QUERY(project)
   const params = {}
 
   const [data] = await client.fetch(query, params)
+
+  const rawData = await fetch(
+    'https://apicdn.sanity.io/files/xmrgv8k7/production/53c8661ad8ad2c7331512b7eb47ec6f58f5cd6b0.gexf'
+  ).then((t) => t.text())
+  const graph = JSON.stringify(gexf.parse(Graph, rawData))
+
+  data.story_chapters[0].dataset = graph
 
   return {
     props: {
