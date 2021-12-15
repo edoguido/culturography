7a405fd4ed30b1b5a00a009469ebcac68acdbf4c
Edoardo Guido <edoardo.guido.93@gmail.com>
2021-12-15 23:17:47 +0100

    refactor: improve code readability

--- lib/components/organisms/networkComparison/index.tsx
+++ lib/components/organisms/networkComparison/index.tsx
@@ -8,9 +8,12 @@ const SingleNetwork = dynamic(
 // import SingleNetwork from 'components/molecules/singleNetwork'
 import * as Styled from './styled'
 
-import { globalCSSVarToPixels } from 'utils/theme'
+// import { globalCSSVarToPixels } from 'utils/theme'
 import { motionOptions } from '@/const/motionProps'
 import { useVizLayout } from '@/context/vizLayoutContext'
+import ProjectTimeline from 'components/molecules/timeline'
+
+const networks = ['left', 'right']
 
 const NetworkComparison = () => {
   const [layout] = useVizLayout()
@@ -20,21 +23,18 @@ const NetworkComparison = () => {
       initial={false}
       animate={{
         right: layout.read ? layout.sidebarWidth.value : 0,
-        bottom: layout.read
-          ? 0
-          : globalCSSVarToPixels('--timeline-height').value,
+        // bottom: layout.read
+        //   ? 0
+        //   : globalCSSVarToPixels('--timeline-height').value,
       }}
       transition={motionOptions}
     >
       <Styled.NetworkComparisonContent>
-        <Styled.NetworkComparisonSingleNetworkWrapper>
-          {layout.clusters.left.shapefile && <SingleNetwork accessor="left" />}
-        </Styled.NetworkComparisonSingleNetworkWrapper>
-        {/* <Styled.NetworkComparisonSingleNetworkWrapper>
-          {layout.clusters.right.shapefile && (
-            <SingleNetwork accessor="right" />
-          )}
-        </Styled.NetworkComparisonSingleNetworkWrapper> */}
+        {networks.map((n) => (
+          <Styled.NetworkComparisonSingleNetworkWrapper key={n}>
+            {layout.clusters[n].shapefile && <SingleNetwork accessor={n} />}
+          </Styled.NetworkComparisonSingleNetworkWrapper>
+        ))}
         <ProjectTimeline />
       </Styled.NetworkComparisonContent>
     </Styled.NetworkComparisonWrapper>
