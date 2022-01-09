c94bb640ec2917bfe3b5f04a5a48c2b4295fa3b2
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-01-09 15:08:51 +0100

    refactor: improve CORS usage

--- pages/api/[asset].tsx
+++ pages/api/[asset].tsx
@@ -1,24 +1,24 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// import Cors from 'cors'
-// import initMiddleware from 'utils/init-middleware'
+import Cors from 'cors'
+import initMiddleware from 'utils/init-middleware'
 
-// const cors = initMiddleware(
-//   Cors({
-//     methods: ['GET'],
-//   })
-// )
+const cors = initMiddleware(
+  Cors({
+    methods: ['GET'],
+  })
+)
 
 export default async function handler(req, res) {
   const { asset } = req.query
 
-  // await cors(req, res)
+  await cors(req, res)
 
   const data = await fetch(
     `https://apicdn.sanity.io/files/xmrgv8k7/production/${asset}.json`
-  ).then((r) => r.text())
+  ).then((res) => res.json())
 
-  return res.send(data)
+  res.status(200).json(data)
 }
 
 export const config = {
