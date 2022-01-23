192e5bb6f8b59d54317023d771e9df7a908dcdc6
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-01-23 20:33:58 +0100

    feat: highlight color on scroll and show annotations

--- lib/components/molecules/singleNetwork/index.tsx
+++ lib/components/molecules/singleNetwork/index.tsx
@@ -1,11 +1,20 @@
-import { useEffect, useMemo, useRef, useState, MutableRefObject } from 'react'
+import {
+  useState,
+  useEffect,
+  useRef,
+  useMemo,
+  useCallback,
+  MutableRefObject,
+  Ref,
+} from 'react'
 
+import { Box3, MathUtils, Vector3 } from 'three'
 import { Canvas, OrthographicCameraProps, useFrame } from '@react-three/fiber'
-
 import {
   OrthographicCamera,
   MapControls,
   useContextBridge,
+  Html,
 } from '@react-three/drei'
 
 import * as d3 from 'd3'
@@ -22,7 +31,6 @@ import {
 import {
   round,
   lerp,
-  randomColor,
   datasetCoordsToArrayOfPoints,
   polygonCentroid,
 } from 'utils/math'
@@ -42,13 +50,15 @@ import {
 const MIN_SIMILARITY_THRESHOLD = 0
 
 const SCENE_CENTER = [0, 0]
-const INITIAL_ZOOM = 6
+const INITIAL_ZOOM = 5
+const BASE_POINT_SIZE = 1
 const LERP_FACTOR = 0.1
 
 // sub-types
 
 interface DatasetProps {
   clusters: object[]
+  allClusters: object[]
   extent: { x: number[]; y: number[] }
 }
 
@@ -65,6 +75,8 @@ const SingleNetwork = ({ data, accessor }) => {
   //
   const [fetching, setFetching] = useState<boolean>(true)
   const [dataset, setDataset] = useState<DatasetProps>(null)
+  //
+  const canvasRef = useRef<HTMLCanvasElement>(null)
   //
   const accessorKey: string = `${accessor}_network_shapefile`
   // is this the network on the left? or on the right?
@@ -109,24 +121,17 @@ const SingleNetwork = ({ data, accessor }) => {
     const xExtent = d3.extent(dataset, (d) => d.x)
     const yExtent = d3.extent(dataset, (d) => d.y)
 
-    setDataset({ clusters: groupedDataset, extent: { x: xExtent, y: yExtent } })
+    setDataset({
+      clusters: groupedDataset,
+      allClusters: clusters,
+      extent: { x: xExtent, y: yExtent },
+    })
   }
 
   // initiate app
   useEffect(() => {
     fetchNetwork().then(prepareNetwork).catch(console.error)
   }, [layout.story.chapter])
-
-  // // update canvas on resize
-  // useEffect(() => {
-  //   // should also be called with resizeObserver
-  //   window.addEventListener('resize', handleResize)
-  //   return () => window.removeEventListener('resize', handleResize)
-  // }, [wrapperRef])
-
-  // useEffect(() => {
-  //   updatePointsColor()
-  // }, [layout.story.block])
 
   return (
     <div
@@ -152,10 +157,26 @@ const SingleNetwork = ({ data, accessor }) => {
           Fetching!
         </div>
       )}
-      <Canvas>
+      <div
+        style={{
+          position: 'absolute',
+          zIndex: 20,
+          top: '50%',
+          left: '50%',
+          color: 'red',
+          transform: 'scale(2)',
+        }}
+      >
+        +
+      </div>
+      <Canvas ref={canvasRef}>
         <ContextBridge>
           {dataset && (
-            <Scene dataset={dataset} sourceNetwork={isSourceNetwork} />
+            <Scene
+              dataset={dataset}
+              sourceNetwork={isSourceNetwork}
+              forwardRef={canvasRef}
+            />
           )}
         </ContextBridge>
       </Canvas>
@@ -168,49 +189,141 @@ const SingleNetwork = ({ data, accessor }) => {
 interface SceneProps {
   dataset: DatasetProps
   sourceNetwork: boolean
+  forwardRef: Ref<HTMLCanvasElement>
 }
 
-const Scene = ({ dataset, sourceNetwork }: SceneProps) => {
+const Scene = ({ dataset, sourceNetwork, forwardRef }: SceneProps) => {
   const [layout] = useVizLayout()
 
   //
-  const [targetZoom, setTargetZoom] = useState(INITIAL_ZOOM)
+  const [targetZoom, setTargetZoom] = useState(null)
   const [targetPosition, setTargetPosition] = useState(SCENE_CENTER)
   //
-  const ref = useRef(null)
+  const cameraRef = useRef(null)
   const groupRef = useRef(null)
   //
   const highlightedClusterIndex = layout.networks.highlight
   const highlightedCluster = layout?.clusters[layout.networks.highlight] || null
+
+  // const canvasWidth = forwardRef.current.clientWidth
+  // const canvasHeight = forwardRef.current.clientHeight
+
   //
   // scales
   const xScale = useMemo(
-    () => d3.scaleLinear().domain(dataset.extent.x).range([-50, 50]),
+    () => d3.scaleLinear().domain(dataset.extent.x).range([-55, 55]),
     [dataset]
   )
   const yScale = useMemo(
-    () => d3.scaleLinear().domain(dataset.extent.y).range([-50, 50]),
+    () => d3.scaleLinear().domain(dataset.extent.y).range([-55, 55]),
     [dataset]
   )
 
-  function resetView() {
-    setTargetPosition(SCENE_CENTER)
+  // const colorScale = useMemo(() => {
+  //   () => d3.scale
+  // }, [dataset])
+
+  const allClustersID = dataset.allClusters.map(
+    (d: ClusterObjectProps) => d.cluster_id
+  )
+
+  const colorScale = useMemo(
+    () =>
+      d3
+        .scaleQuantize()
+        .domain(d3.extent(allClustersID))
+        .range(d3.quantize(d3.interpolateTurbo, allClustersID.length)),
+    [allClustersID]
+  )
+
+  const getColor = useCallback(
+    (id) => {
+      // we're not highlighting any cluster at the moment
+      if (!highlightedClusterIndex) return colorScale(id)
+
+      // otherwise we're highlighting a cluster
+      if (id === highlightedCluster.cluster_id) return colorScale(id)
+
+      // but if we're not in the source network, we must find
+      // source network highlighted cluster similarities
+
+      const similarityWithHighlightedCluster =
+        highlightedCluster.similarities[id]
+
+      if (
+        similarityWithHighlightedCluster &&
+        similarityWithHighlightedCluster > MIN_SIMILARITY_THRESHOLD
+      ) {
+        return colorScale(id)
+      }
+
+      return '#333333'
+    },
+    [colorScale]
+  )
+
+  // here we'll highlight cluster in explore mode
+  function handleClusterClick(e) {
+    function animateToRandomPoint() {
+      const randZ = round(INITIAL_ZOOM + Math.random() * 100)
+
+      const [randX, randY] = [
+        round((Math.random() - 0.5) * 15),
+        round((Math.random() - 0.5) * 15),
+      ]
+
+      moveTo({ location: [randX, randY], zoom: randZ })
+    }
+
+    animateToRandomPoint()
+  }
+
+  // we should set initial zoom here
+  function setInitialZoom() {
+    // const boundingBox = new Box3()
+    // boundingBox.setFromObject(groupRef.current)
+
+    // const size = boundingBox.getSize(new Vector3())
+    // // const center = boundingBox.getCenter(new Vector3())
+
+    // const maxDim = Math.max(size.x, size.y, size.z)
+    // const fov = 6 * (Math.PI / 180)
+
+    // const cameraZ = Math.abs((maxDim / 4) * Math.tan(fov * 2))
+
     setTargetZoom(INITIAL_ZOOM)
   }
 
+  function resetView() {
+    setTargetPosition(SCENE_CENTER)
+    setInitialZoom()
+  }
+
+  function moveTo({ location, zoom }) {
+    const targetLocation = [location[0], location[1]]
+
+    setTargetPosition(targetLocation)
+    setTargetZoom(zoom)
+  }
+
+  // handle change of block and chapter
   useEffect(() => {
     if (!highlightedCluster) {
       resetView()
       return
     }
 
+    const zoomedIn = 0
+
     if (sourceNetwork) {
       // if sourceNetwork we move to its cluster centroid...
-      const { centroid } = highlightedCluster
-      const screenCoordsCentroid = [xScale(centroid[0]), yScale(centroid[1])]
+      const {
+        centroid: [cx, cy],
+      } = highlightedCluster
 
-      setTargetPosition(screenCoordsCentroid)
-      setTargetZoom(15)
+      const screenCoordsCentroid = [xScale(cx), yScale(cy)]
+
+      moveTo({ location: screenCoordsCentroid, zoom: zoomedIn })
       return
     }
 
@@ -230,27 +343,30 @@ const Scene = ({ dataset, sourceNetwork }: SceneProps) => {
       (c: ClusterObjectProps) => c.centroid
     )
 
-    const [x, y] = polygonCentroid(matchingClustersCentroids)
+    // const [x, y] = polygonCentroid(matchingClustersCentroids)
+
+    const [x, y] = matchingClustersCentroids[0]
+
     const targetCenterRescaled = [xScale(x), yScale(y)]
 
-    setTargetPosition(targetCenterRescaled)
-    setTargetZoom(15)
-
-    // setTargetZoom(centroid)
+    moveTo({ location: targetCenterRescaled, zoom: zoomedIn })
   }, [highlightedClusterIndex])
 
+  //
+  // **
+  // runtime operations
+
   useFrame(() => {
-    const z = ref.current.zoom
+    const z = cameraRef.current.zoom
     const currentZoom = round(z)
-    // const isAtTargetZoom = currentZoom === round(targetZoom)
+    const isAtTargetZoom = currentZoom === round(targetZoom)
 
     if (targetZoom) {
-      ref.current.zoom = lerp(z, targetZoom, LERP_FACTOR)
-      ref.current.updateProjectionMatrix()
-    } else {
+      cameraRef.current.zoom = lerp(z, targetZoom, LERP_FACTOR)
+      cameraRef.current.updateProjectionMatrix()
+    } else if (isAtTargetZoom && targetZoom !== null) {
       setTargetZoom(null)
     }
-
     const [x, y] = [groupRef.current.position.x, groupRef.current.position.y]
     const isAtTargetPosition =
       round(x) === targetPosition[0] && round(y) === targetPosition[1]
@@ -260,50 +376,49 @@ const Scene = ({ dataset, sourceNetwork }: SceneProps) => {
     }
   })
 
-  function handleClusterClick(e) {
-    function animateToRandomPoint() {
-      const randZ = round(INITIAL_ZOOM + Math.random() * 100)
-      setTargetZoom(randZ)
-
-      const [randX, randY] = [
-        round((Math.random() - 0.5) * 15),
-        round((Math.random() - 0.5) * 15),
-      ]
-      setTargetPosition([randX, randY])
-    }
-
-    animateToRandomPoint()
-  }
-
   return (
     <>
       <OrthographicCamera
-        ref={ref}
+        ref={cameraRef}
         makeDefault
-        position={[0, 0, 0]}
+        position={[0, 0, -10]}
         zoom={INITIAL_ZOOM}
-        up={[0, 0, 1]}
+        // up={[0, 0, 1]}
       />
       <MapControls
         enabled={false}
-        // enableRotate={false}
-        // enableZoom={true}
-        // enableDamping
+        enablePan
+        enableRotate
+        enableZoom
+        enableDamping
       />
-      <ambientLight intensity={0.5} />
+      {/* <ambientLight intensity={0.5} />
       <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />
-      <group ref={groupRef} position={[0, 0, 0]}>
+      <pointLight position={[-10, -10, -10]} /> */}
+      <group
+        ref={groupRef}
+        position={[0, 0, 10]}
+        rotation={[
+          MathUtils.degToRad(0),
+          MathUtils.degToRad(0),
+          MathUtils.degToRad(90),
+        ]}
+      >
         {dataset &&
-          dataset.clusters.map((d, i) => (
-            <Cluster
-              key={i}
-              data={d}
-              scales={{ xScale, yScale }}
-              cameraRef={ref}
-              // onClick={handleClusterClick}
-            />
-          ))}
+          dataset.clusters.map((d: ClusterObjectProps, i) => {
+            const color = getColor(d.cluster_id)
+
+            return (
+              <Cluster
+                key={i}
+                data={d}
+                scales={{ xScale, yScale }}
+                color={color}
+                cameraRef={cameraRef}
+                // onClick={handleClusterClick}
+              />
+            )
+          })}
       </group>
     </>
   )
@@ -315,26 +430,25 @@ interface ClusterProps {
   key: number
   data: {
     cluster_id?: number
+    centroid?: [number, number]
     nodes?: any[]
   }
   scales: {
-    xScale: () => void
-    yScale: () => void
+    xScale: (a: number) => number
+    yScale: (a: number) => number
   }
+  color: string | number
   cameraRef: MutableRefObject<OrthographicCameraProps>
   onClick?: (e: any) => void
 }
 
-const Cluster = ({ data, scales, cameraRef, onClick }: ClusterProps) => {
+const Cluster = ({ data, scales, color, cameraRef, onClick }: ClusterProps) => {
   const { xScale, yScale } = scales
 
-  const pointRef = useRef(null)
-  const bufferRef = useRef(null)
+  const [labelPosition, setLabelPosition] = useState(null)
 
-  const baseSize = 1
-  const color = useMemo(() => randomColor(), [])
-
-  const clusterId = data.cluster_id
+  const pointsRef = useRef(null)
+  const pointMaterialRef = useRef(null)
 
   const subDataset = data.nodes
   const numPoints = subDataset.length
@@ -344,12 +458,28 @@ const Cluster = ({ data, scales, cameraRef, onClick }: ClusterProps) => {
     [subDataset, numPoints]
   )
 
-  useFrame(({ clock }) => {
-    //pointRef.current.size = lerp(pointRef.current.size, baseSize + cameraRef.current.zoom / 10, LERP_FACTOR)
-    pointRef.current.size = baseSize + cameraRef.current.zoom / 20
+  const [cx, cy] = data.centroid
+  const [ax, ay] = [xScale(cx), yScale(cy)]
+
+  useEffect(() => {
+    if (!pointsRef.current) return
+
+    const boundingBox = new Box3()
+    boundingBox.setFromObject(pointsRef.current)
+
+    // const size = boundingBox.getSize(new Vector3())
+    const center = boundingBox.getCenter(new Vector3())
+
+    setLabelPosition(center)
+  }, [pointsRef.current])
+
+  useFrame(() => {
+    //pointMaterialRef.current.size = lerp(pointMaterialRef.current.size, BASE_POINT_SIZE + cameraRef.current.zoom / 10, LERP_FACTOR)
+    pointMaterialRef.current.size =
+      BASE_POINT_SIZE + cameraRef.current.zoom / 20
     // // set material color
     // if (clusterId === 0) {
-    //   pointRef.current.color = new Color(
+    //   pointMaterialRef.current.color = new Color(
     //     Math.sin(clock.getElapsedTime()),
     //     Math.sin(clock.getElapsedTime()),
     //     Math.cos(clock.getElapsedTime())
@@ -365,12 +495,38 @@ const Cluster = ({ data, scales, cameraRef, onClick }: ClusterProps) => {
     document.documentElement.style.cursor = ''
   } */
 
+  // const normalizeCoords = ([x,y]) => {
+  //   return []
+  // }
+
   return (
     <group onClick={onClick}>
-      <points>
+      {labelPosition && (
+        <group position={labelPosition}>
+          <Html
+            as="div" // Wrapping element (default: 'div')
+            // wrapperClass // The className of the wrapping element (default: undefined)
+            // prepend // Project content behind the canvas (default: false)
+            center // Adds a -50%/-50% css transform (default: false) [ignored in transform mode]
+            // fullscreen // Aligns to the upper-left corner, fills the screen (default:false) [ignored in transform mode]
+            distanceFactor={30} // If set (default: undefined), children will be scaled by this factor, and also by distance to a PerspectiveCamera / zoom by a OrthographicCamera.
+            zIndexRange={[100, 0]} // Z-order range (default=[16777271, 0])
+            // portal={domnodeRef} // Reference to target container (default=undefined)
+            transform // If true, applies matrix3d transformations (default=false)
+            sprite // Renders as sprite, but only in transform mode (default=false)
+            // calculatePosition={() => [ax, ay]} // Override default positioning function. (default=undefined) [ignored in transform mode]
+            // occlude={[ref]} // Can be true or a Ref<Object3D>[], true occludes the entire scene (default: undefined)
+            // onOcclude={(visible) => null} // Callback when the visibility changes (default: undefined)
+            // {...groupProps} // All THREE.Group props are valid
+            // {...divProps} // All HTMLDivElement props are valid
+          >
+            <h2>{data.cluster_id}</h2>
+          </Html>
+        </group>
+      )}
+      <points ref={pointsRef}>
         <bufferGeometry>
           <bufferAttribute
-            ref={bufferRef}
             attachObject={['attributes', 'position']}
             count={numPoints}
             itemSize={3}
@@ -378,9 +534,9 @@ const Cluster = ({ data, scales, cameraRef, onClick }: ClusterProps) => {
           />
         </bufferGeometry>
         <pointsMaterial
-          ref={pointRef}
+          ref={pointMaterialRef}
           color={color}
-          size={baseSize}
+          size={BASE_POINT_SIZE}
           /* sizeAttenuation={true} alphaTest={0.5} transparent={true} */
         />
       </points>
