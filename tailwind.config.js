c21d74f1e20acf2b6b014f84c658fa2315154388
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-02-06 16:48:39 +0100

    build: update packages list

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    // './pages/**/*.{js,ts,jsx,tsx}',
+    './lib/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        roskildeOrange: '#ee7203ff',
+        paletteGreen: '#45f194ff',
+        paletteLightBlue: '#4fccf4ff',
+        paletteViolet: '#c17df7ff',
+      },
+    },
+  },
+  plugins: [],
+}
