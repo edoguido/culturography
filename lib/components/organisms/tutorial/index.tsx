f58c806009778e19be2aa00a1a022e270a74d094
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-02-20 22:51:54 +0000

    feat: add tutorial embed from Figma

---
+++ lib/components/organisms/tutorial/index.tsx
@@ -0,0 +1,81 @@
+import { useRef, useState } from 'react'
+import { motion } from 'framer-motion'
+//
+import Spinner from 'components/atoms/spinner'
+
+const Tutorial = ({ onClose }) => {
+  const [isLoaded, setIsLoaded] = useState(false)
+  const ref = useRef<HTMLIFrameElement>()
+
+  return (
+    <motion.div
+      className="fixed z-50 top-0 left-0 bottom-0 right-0 w-full flex justify-center items-center"
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={{
+        initial: {
+          opacity: 0,
+          transition: {
+            when: 'beforeChildren',
+          },
+        },
+        animate: {
+          opacity: 1,
+          transition: {
+            when: 'beforeChildren',
+          },
+        },
+        exit: {
+          opacity: 0,
+          transition: {
+            when: 'afterChildren',
+          },
+        },
+      }}
+    >
+      <div className="relative z-10 flex flex-col justify-center w-full h-full">
+        <button
+          className="relative z-10 h-[var(--nav-height)] text-white"
+          onClick={onClose}
+        >
+          Close
+        </button>
+        <motion.div
+          className="relative z-10 w-full h-full rounded-lg flex justify-center items-center"
+          variants={{
+            initial: {
+              opacity: 0,
+              y: 80,
+            },
+            animate: {
+              opacity: 1,
+              y: 0,
+            },
+            exit: {
+              opacity: 0,
+              y: 80,
+            },
+          }}
+          transition={{
+            type: 'ease',
+            ease: [1, 0, 0, 1],
+          }}
+        >
+          {!isLoaded && <Spinner />}
+          <iframe
+            ref={ref}
+            width="100%"
+            height="100%"
+            src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2F38IPI1qJGKVA1ORVF8zYPf%2FCultural-Impact-Index%3Fpage-id%3D405%253A2895%26node-id=405%3A3094%26viewport%3D262%252C48%252C0.06%26scaling%3Dcontain%26starting-point-node-id%3D405%253A3094%26hotspot-hints%3D0"
+            allowFullScreen
+            onLoad={() => setIsLoaded(true)}
+          ></iframe>
+        </motion.div>
+      </div>
+      <div className="absolute inset-0 bg-[#000000e0]" />
+    </motion.div>
+  )
+}
+
+export default Tutorial
