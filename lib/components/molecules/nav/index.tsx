7f4770eb831af8c7663aeff481503528affd233e
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-03-03 17:07:13 +0100

    feat: show tutorial label on first story load

--- lib/components/molecules/nav/index.tsx
+++ lib/components/molecules/nav/index.tsx
@@ -1,21 +1,24 @@
-import Tutorial from 'components/organisms/tutorial'
-import { AnimatePresence } from 'framer-motion'
+import { useState } from 'react'
 import Link from 'next/link'
-import { useEffect, useState } from 'react'
+import { AnimatePresence, motion } from 'framer-motion'
+
+import Tutorial from 'components/organisms/tutorial'
 import NetworkComparisonAnnouncer from '../networkComparisonAnnouncer'
 
 // import ModeSwitcher from 'components/atoms/modeSwitcher'
 
 const Nav = ({ title }) => {
   const [tutorial, setTutorial] = useState<boolean>(false)
+  const userHasViewedTutorial = localStorage.getItem('userHasViewedTutorial')
 
-  useEffect(() => {
-    const userHasViewedTutorial = localStorage.getItem('userHasViewedTutorial')
-
-    if (!userHasViewedTutorial) setTutorial(true)
-
+  function showTutorial() {
+    setTutorial(true)
     localStorage.setItem('userHasViewedTutorial', '1')
-  }, [])
+  }
+
+  function hideTutorial() {
+    setTutorial(false)
+  }
 
   return (
     <div className="fixed top-0 right-0 bottom-0 left-0 h-[var(--nav-height)] z-[999] px-2">
@@ -33,15 +36,26 @@ const Nav = ({ title }) => {
         </div>
         <div>
           <button
-            className="cursor-pointer rounded-full border-2 border-white w-7 h-7"
-            onClick={() => setTutorial(true)}
+            className="flex cursor-pointer rounded-full border-2 border-white w-auto py-1 px-3"
+            onClick={showTutorial}
           >
-            ?
+            <motion.div
+              initial={{
+                y: 20,
+                opacity: 0,
+              }}
+              animate={{
+                y: 0,
+                opacity: 1,
+              }}
+            >
+              {!userHasViewedTutorial ? 'Click here for tutorial' : '?'}
+            </motion.div>
           </button>
         </div>
       </div>
       <AnimatePresence>
-        {tutorial && <Tutorial onClose={() => setTutorial(false)} />}
+        {tutorial && <Tutorial onClose={hideTutorial} />}
       </AnimatePresence>
     </div>
   )
