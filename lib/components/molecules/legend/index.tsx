9003e9a2042daf6f73e026ab6ae1212c6890a677
Edoardo Guido <edoardo.guido.93@gmail.com>
2022-03-03 16:33:01 +0100

    feat: add working legend

--- lib/components/molecules/legend/index.tsx
+++ lib/components/molecules/legend/index.tsx
@@ -1,12 +1,7 @@
 import { useVizLayout } from '@/context/vizLayoutContext'
+import { AnimatePresence, motion } from 'framer-motion'
 
-const labels = [
-  'No overlap',
-  'Very weak overlap',
-  'Weak overlap',
-  'Strong overlap',
-  'Very strong overlap',
-]
+const labels = ['None', 'Very weak', 'Weak', 'Strong', 'Very strong']
 
 const Legend = () => {
   const [layout] = useVizLayout()
@@ -14,18 +9,51 @@ const Legend = () => {
   const legend = layout?.networks?.legend
 
   return (
-    legend && (
-      <div className="absolute z-10 right-0 top-0 text-white bg-[#ffffff30] p-2">
-        <div>Overlap</div>
-        {legend.map((color, i) => {
-          return (
-            <span key={i} className="text-sm">
-              {labels[i]}
-            </span>
-          )
-        })}
-      </div>
-    )
+    <AnimatePresence>
+      {legend && (
+        <motion.div
+          initial="initial"
+          animate="animate"
+          exit="exit"
+          variants={{
+            initial: {
+              y: 20,
+              opacity: 0,
+            },
+            animate: {
+              y: 0,
+              opacity: 1,
+            },
+            exit: {
+              y: 20,
+              opacity: 0,
+            },
+          }}
+          transition={{
+            type: 'ease',
+            ease: [0, 0, 0, 1],
+          }}
+          className="absolute z-10 left-0 bottom-0 origin-bottom-left text-white bg-white bg-opacity-10 rounded-tr-lg p-4"
+        >
+          <div>Overlap</div>
+          <div className="flex mt-2">
+            {legend.map((color, i) => {
+              return (
+                <div className="w-20 mr-2 last:mr-0 flex flex-col">
+                  <div
+                    className="w-full h-4 rounded-sm"
+                    style={{ backgroundColor: color }}
+                  />
+                  <span key={i} className="mt-2 text-xs">
+                    {labels[i]}
+                  </span>
+                </div>
+              )
+            })}
+          </div>
+        </motion.div>
+      )}
+    </AnimatePresence>
   )
 }
 
--- lib/components/molecules/nav/index.tsx
+++ lib/components/molecules/nav/index.tsx
@@ -2,10 +2,9 @@ import Tutorial from 'components/organisms/tutorial'
 import { AnimatePresence } from 'framer-motion'
 import Link from 'next/link'
 import { useEffect, useState } from 'react'
+import NetworkComparisonAnnouncer from '../networkComparisonAnnouncer'
 
 // import ModeSwitcher from 'components/atoms/modeSwitcher'
-
-import * as Styled from './styled'
 
 const Nav = ({ title }) => {
   const [tutorial, setTutorial] = useState<boolean>(false)
@@ -19,27 +18,32 @@ const Nav = ({ title }) => {
   }, [])
 
   return (
-    <Styled.NavWrapper>
-      <Styled.NavContent>
-        <Styled.NavProjectHeaderSection>
+    <div className="fixed top-0 right-0 bottom-0 left-0 h-[var(--nav-height)] z-[999] px-2">
+      <div className="h-full flex justify-between items-center">
+        <div className="flex items-center m-0">
           <Link href="/" passHref>
-            <Styled.NavLogo>Culturographies</Styled.NavLogo>
+            <a>
+              <h1 className="text-lg">Culturographies</h1>
+            </a>
           </Link>
-          <Styled.NavProjectName>{title}</Styled.NavProjectName>
-        </Styled.NavProjectHeaderSection>
-        <Styled.NavSection>
+          <h2 className="bg-white text-black text-lg py-1 px-4 rounded-full mx-2">
+            {title}
+          </h2>
+          <NetworkComparisonAnnouncer />
+        </div>
+        <div>
           <button
             className="cursor-pointer rounded-full border-2 border-white w-7 h-7"
             onClick={() => setTutorial(true)}
           >
             ?
           </button>
-        </Styled.NavSection>
-      </Styled.NavContent>
+        </div>
+      </div>
       <AnimatePresence>
         {tutorial && <Tutorial onClose={() => setTutorial(false)} />}
       </AnimatePresence>
-    </Styled.NavWrapper>
+    </div>
   )
 }
 
