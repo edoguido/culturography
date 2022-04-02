4355eccf676930b50f8a7b32932daadf47d84742
edoguido <edoardo.guido.93@gmail.com>
2022-04-02 16:51:07 +0100

    refactor: delete unused code in main App component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,7 @@
-import { Fragment } from 'react'
-
-import { GlobalStyle } from '../lib/utils/theme'
-// import theme from '../public/design-tokens.json'
-
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </Fragment>
-  )
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
 }
 
-export default MyApp
+export default App
