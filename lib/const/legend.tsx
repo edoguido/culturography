6690721de826b160ce9bb8ec3b0357aea6ff360a
edoguido <edoardo.guido.93@gmail.com>
2022-06-26 21:29:31 +0100

    feat: add types

--- lib/const/legend.tsx
+++ lib/const/legend.tsx
@@ -1,4 +1,4 @@
-const LEGEND_LABELS = [
+const LEGEND_LABELS: string[] = [
   'None',
   // 'Very weak',
   'Weak',
@@ -6,6 +6,6 @@ const LEGEND_LABELS = [
   'Strong',
   // 'Very strong',
 ]
-const LEGEND_NUM_STEPS = LEGEND_LABELS.length - 1
+const LEGEND_NUM_STEPS: number = LEGEND_LABELS.length - 1
 
 export { LEGEND_LABELS, LEGEND_NUM_STEPS }
